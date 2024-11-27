710fec7910ce11606bc6249828e2133fbc684ca3
Sören Möller <soeren-moeller99@gmx.de>
2024-11-27 20:04:21 +0100

    tmp landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 
+    class="flex justify-center mt-20 font-bold text-2xl"
+>
+    Work in Progress...
+</h1>
